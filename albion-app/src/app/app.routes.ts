4ba4f470bd4bc0f13f91cacd3a2462cd6decaa10
Efelante-Volador <fabianlopezmagnan@mail.com>
2024-08-13 15:54:25 -0400

    Actualizacion basicas de Vistas

--- albion-app/src/app/app.routes.ts
+++ albion-app/src/app/app.routes.ts
@@ -4,12 +4,15 @@ import { LoginComponent} from './vistas/login/login.component';
 import { LobbyComponent} from './vistas/lobby/lobby.component';
 import { ExampleComponent} from './vistas/example/example.component';
 
+
 import { NgModule } from '@angular/core';
 export const routes: Routes = [
     {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent },
     {path: 'login', component: LoginComponent},
     {path: 'lobby', component: LobbyComponent},
     {path: 'example', component: ExampleComponent},
+    {path: '**', redirectTo: '/home' }
 ];
 
 @NgModule({
