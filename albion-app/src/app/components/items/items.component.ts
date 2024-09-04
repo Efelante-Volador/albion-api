3ee2cb54aa6ea836c3bb340f1fd4c0c086d6ac84
Efelante-Volador <fabianlopezmagnan@mail.com>
2024-09-04 15:30:45 -0400

    Cambios Uso de api

--- albion-app/src/app/components/items/items.component.ts
+++ albion-app/src/app/components/items/items.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Importa FormsModule aquí
 import { ItemsService } from '../../service/items.service'; // Ajusta la ruta según la estructura de tu proyecto
 
 @Component({
   selector: 'app-items',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule], // Agrega FormsModule aquí
   templateUrl: './items.component.html',
   styleUrls: ['./items.component.css']
 })
 export class ItemsComponent implements OnInit {
   items: any[] = [];
+  selectedItem: any; // Variable para almacenar el ítem seleccionado
 
   constructor(private itemsService: ItemsService) { }
 
   ngOnInit(): void {
-    
+    this.itemsService.getItems().subscribe(
+      (data) => this.items = data, // Asigna los datos obtenidos al array `items`
+      (error) => console.error('Error al obtener tareas', error) // Manejo de errores
+    );
   }
 }
--- albion-app/src/app/service/items.service.ts
+++ albion-app/src/app/service/items.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemsService {
+  private apiUrl = 'https://jsonplaceholder.typicode.com/todos'; // URL de la API
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getItems(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
 }
